38712e93e1ee103d2f212415f5354155b846f8cd
FilipMikulski <mikulskifilip@gmail.com>
2018-08-13 12:10:44 +0200

    asd1

--- src/main/java/pl/coderslab/model/Customer.java
+++ src/main/java/pl/coderslab/model/Customer.java
@@ -1,4 +1,51 @@
 package pl.coderslab.model;
 
+import java.util.Date;
+
 public class Customer {
+    String name;
+    String surename;
+    Date birthday;
+    int id;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String surename, Date birthday) {
+        this.name = name;
+        this.surename = surename;
+        this.birthday = birthday;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurename() {
+        return surename;
+    }
+
+    public void setSurename(String surename) {
+        this.surename = surename;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/pl/coderslab/model/Employee.java
+++ src/main/java/pl/coderslab/model/Employee.java
@@ -1,4 +1,79 @@
 package pl.coderslab.model;
 
 public class Employee {
+    String name;
+    String surname;
+    String adress;
+    int phonenumber;
+    String note;
+    float workhours;
+    int id;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surname, String adress, int phonenumber, String note, float workhours) {
+        this.name = name;
+        this.surname = surname;
+        this.adress = adress;
+        this.phonenumber = phonenumber;
+        this.note = note;
+        this.workhours = workhours;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public void setWorkhours(float workhours) {
+        this.workhours = workhours;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public float getWorkhours() {
+        return workhours;
+    }
 }
