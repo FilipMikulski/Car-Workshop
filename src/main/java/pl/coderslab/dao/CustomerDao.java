906ccc902bfd3703544312e24b22630ca18b0b10
FilipMikulski <mikulskifilip@gmail.com>
2018-08-13 12:29:21 +0200

    zrobione dao dla customer i employee

--- src/main/java/pl/coderslab/dao/CustomerDao.java
+++ src/main/java/pl/coderslab/dao/CustomerDao.java
@@ -1,4 +1,26 @@
 package pl.coderslab.dao;
 
+import pl.coderslab.model.Customer;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
 public class CustomerDao {
+    public static void saveToDb(Customer customer) {
+
+    }
+
+    public static void deleteCustomer(int id) throws SQLException {
+
+    }
+
+    public static ArrayList<Customer> loadAll() {
+        return null;
+    }
+
+    public static Customer loadById(int id) {
+        return null;
+    }
 }
+
--- src/main/java/pl/coderslab/dao/EmployeeDao.java
+++ src/main/java/pl/coderslab/dao/EmployeeDao.java
@@ -1,4 +1,25 @@
 package pl.coderslab.dao;
 
+import pl.coderslab.model.Employee;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class EmployeeDao {
+    public static void saveToDb(Employee employee) {
+
+    }
+
+    public static void deleteEmployee(int id) throws SQLException {
+
+    }
+
+    public static ArrayList<Employee> loadAll() {
+        return null;
+    }
+
+    public static Employee loadById(int id) {
+        return null;
+    }
 }
+
